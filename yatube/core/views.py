2bf50b7163771e60a2f6deaa3ba5df2a3f004ea9
Renat Emaletdinov <spamhatertom@gmail.com>
2022-11-18 21:50:09 +0300

    another fix

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -7,17 +7,21 @@ def page_not_found(request, exception):
         request,
         'core/404.html',
         {'path': request.path},
-        HTTPStatus.NOT_FOUND
+        status=HTTPStatus.NOT_FOUND
     )
 
 
 def server_error(request):
-    return render(request, 'core/500.html', HTTPStatus.INTERNAL_SERVER_ERROR)
+    return render(
+        request,
+        'core/500.html',
+        status=HTTPStatus.INTERNAL_SERVER_ERROR
+    )
 
 
 def permission_denied(request, exception):
-    return render(request, 'core/403.html', HTTPStatus.FORBIDDEN)
+    return render(request, 'core/403.html', status=HTTPStatus.FORBIDDEN)
 
 
 def csrf_failure(request, reason=''):
-    return render(request, 'core/403csrf.html', HTTPStatus.FORBIDDEN)
+    return render(request, 'core/403csrf.html', status=HTTPStatus.FORBIDDEN)
